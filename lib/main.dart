2733e0f8337967fa0af9eac05c53bc0994fc6724
KevinBenaya <darkfaust063@gmail.com>
2022-03-20 18:57:09 +0700

    Add SecondRoute class

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,24 @@ class FirstRoute extends StatelessWidget {
     );
   }
 }
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
