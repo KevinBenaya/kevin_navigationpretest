798452daf09a1fb4a050be0642a2f9eb5f03282c
KevinBenaya <darkfaust063@gmail.com>
2022-03-20 18:54:39 +0700

    Add Class FirstRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const HeroApp());
-
-class HeroApp extends StatelessWidget {
-  const HeroApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Transition Demo',
-      home: MainScreen(),
-    );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main Screen'),
+        title: const Text('First Route'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return const DetailScreen();
-          }));
-        },
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.network('https://picsum.photos/250?image=9'),
-        ),
-      ),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Center(
-          child: Hero(
-            tag: 'imageHero',
-            child: Image.network(
-              'https://picsum.photos/250?image=9',
-            ),
-          ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open Route'),
+          onPressed: () {},
         ),
       ),
     );
