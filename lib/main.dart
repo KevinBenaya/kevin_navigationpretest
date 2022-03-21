c381167a5ca2619928bcdc91ed0bfbd19d6786d8
KevinBenaya <darkfaust063@gmail.com>
2022-03-21 12:08:49 +0700

    add Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Passing Data',
+      home: TodosScreen(
+        todos: List.generate(
+          20,
+          (i) => Todo(
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
 class Todo {
   final String title;
   final String description;
