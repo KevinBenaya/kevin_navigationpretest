f8c51a23a75b65518395a367fbdad73b07676631
KevinBenaya <darkfaust063@gmail.com>
2022-03-21 10:16:19 +0700

    Add SecondScreen

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,22 @@ class FirstScreen extends StatelessWidget {
     );
   }
 }
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
