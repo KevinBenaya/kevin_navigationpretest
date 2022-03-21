cb625113e79ee797a26c79350806f1eb1fe98723
KevinBenaya <darkfaust063@gmail.com>
2022-03-21 11:38:20 +0700

    Add HomeScreen but still error

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  ExtractArgumentScreen.routeName,
+                  arguments: ScreenArguments(),
+                );
+              },
+              child: const Text('Navigate to screen that extracts arguments'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  PassArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Accept Arguments Screen',
+                    'This message is extracted in the onGenerateRoute '
+                        'function.',
+                  ),
+                );
+              },
+              child: const Text('Navigate to a named that accepts arguments'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class ScreenArguments {
   final String title;
   final String message;
