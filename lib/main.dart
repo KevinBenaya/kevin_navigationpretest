f4c2ea9e9aadbdd74c88f84e29a9922f44ad5a32
KevinBenaya <darkfaust063@gmail.com>
2022-03-21 12:13:38 +0700

    add DetailScreen

--- lib/main.dart
+++ lib/main.dart
@@ -54,3 +54,22 @@ class TodosScreen extends StatelessWidget {
     );
   }
 }
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key, required this.todo}) : super(key: key);
+
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}
