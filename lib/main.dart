e3c574212c865fe5dc878edcdf9b88d6bd54c89f
KevinBenaya <darkfaust063@gmail.com>
2022-03-21 12:10:33 +0700

    add TodoScreen Class

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,34 @@ class Todo {
 
   const Todo(this.title, this.description);
 }
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
